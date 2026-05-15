0bb210969b19c7f737f1a83b2d9e4a26fa44c447
mohamedhany01 <61619208+mohamedhany01@users.noreply.github.com>
2026-05-15 21:17:05 +0300

    refactor: phase 2 finish lexer to dom migration
    Story: Completed the migration by staging the deletions of the old lexer directory files. All logic has been moved to the dom directory.

--- src/lexer/Lexeme.cpp
+++
@@ -1,8 +0,0 @@
-#include "Lexeme.h"
-
-/**
- * Story: Updates the internal focus state.
- * Use-case: Used by the Tab manager to track which element receives keyboard
- * input.
- */
-void Lexeme::set_focused(bool is_focused) { is_focused_ = is_focused; }
